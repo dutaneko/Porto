fef592f464049ef3c7838c7e835bf0e97ae14457
DutaNEKO <78887087+dutaneko@users.noreply.github.com>
2025-09-22 14:01:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle Navbar Mobile
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
